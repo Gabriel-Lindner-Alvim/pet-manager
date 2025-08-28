8de35b824e6bc52c5177650031de4ca3898e63a7
Gabriel Lindner Alvim <131921503+Gabriel-Lindner-Alvim@users.noreply.github.com>
2025-08-28 16:51:01 -0300

    mockup base da tela inicial do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold text-white">
-        Tailwind funcionando! 🎉
-      </h1>
-    
+    <div className="min-h-screen bg-base-200">    
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">🐾 Pet Manager</a>
+        </div>
+        <div className="flex gap-2">
+          <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+          <div className="dropdown dropdown-end">
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li><a>Settings</a></li>
+              <li><a>Logout</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+       <div className="p-6 flex justify-center">
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Bem-vindo!</h2>
+            <p>Gerencie seus pets, vacinas, remédios e muito mais.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Adicionar Pet</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+
   )
 }
 
-export default App
